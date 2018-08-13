3ec597d45ed475652edb5f3b4868a8bfb85d803b
Samir Gossain <samir.gossain@gmail.com>
2018-08-13 16:48:09 +0100

    Auth working

--- spec/auth_spec.rb
+++ spec/auth_spec.rb
@@ -1,4 +1,30 @@
 require "auth"
 
+
 describe Auth do
+  before(:each) do
+    @authorizer = Auth.new
+    # @valid_username = "your_username"
+    # @valid_password = "5up3r$ecretKey!"
+    # @invalid_username = "invalid"
+    # @invalid_password = "invalid_and_wrong"
+    @valid = ["your_username", "5up3r$ecretKey!"]
+    @invalid = ["invalid", "invalid_and_wrong"]
+  end
+
+  describe ".authenticate" do
+    context "given InValid Username and/or Password" do
+      it "returns 404" do
+        expect(@authorizer.authenticate(@invalid[0], @invalid[1])).to be_nil
+      end
+    end
+  end
+
+  describe ".authenticate" do
+    context "given Valid Username and/or Password" do
+      it "returns a random token" do
+        expect(@authorizer.authenticate(@valid[0], @valid[1]).code).to eql(200)
+      end
+    end
+  end
 end
--- test.rb
+++ test.rb
@@ -6,19 +6,23 @@ require 'rest_client'
 #   "username": "your_username",
 #   "apikey": "5up3r$ecretKey!"
 # }'
-
+username = "your_username"
+apikey = "5up3r$ecretKey!"
 # CUSTOM VALUES
+username = "\"#{username}\""
+apikey = "\"#{apikey}\""
 values = '{
-  "username": "SamirG",
-  "apikey": "ADA8772865C0CA3C"
+  "username":'+username+',
+  "apikey": '+apikey+'
 }'
 
 headers = {
   :content_type => 'application/json'
 }
-
+puts values
 response = RestClient.post 'https://coolpay.herokuapp.com/api/login', values, headers
 puts response
 
 # Username: <SamirG>
 # API Key: <ADA8772865C0CA3C>
+# "{\n  \"username\": \"your_username\",\n  \"apikey\": \"5up3r$ecretKey!\"\n}
