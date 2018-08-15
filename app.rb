2184399047f2cf2af8c19bc45d58c08d74968e7c
Samir Gossain <samir.gossain@gmail.com>
2018-08-15 20:24:26 +0100

    created home buttons

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require './lib/payment_handler'
+require './lib/recipient_handler'
 require './lib/auth'
 
 
@@ -7,15 +8,32 @@ get '/' do
   erb :login
 end
 
+
+get '/home' do
+  erb :home
+end
+
 post '/home' do
   @@auth = Auth.new
   @@auth.authenticate(params[:username], params[:apikey])
   if Auth.authorized
-    erb :home, :locals => {:username => params[:username], :apikey => params[:apikey]}
+    erb :home
   else
     erb :fail
   end
 end
+
+
+get '/recipients/add' do
+  erb :create_recipient
+end
+
+post '/recipients/add' do
+  recipientHandler = RecipientHandler.new
+  @ans = recipientHandler.add_recipient(@@auth, params[:recipientname])
+  erb :confirm_recipient
+end
+
 
 get '/payments/show' do
   paymentHandler = PaymentHandler.new
@@ -23,20 +41,13 @@ get '/payments/show' do
   erb :payments
 end
 
-get '/recipients/add' do
-  erb :create_recipient
-end
-
-post '/recipients/add' do
-  "Recipient " + params[:recipientname] + " Added!"
-  # erb :confirm_recipient, :locals => {:recipientname => params[:username]}
-end
 
 get '/payments/add' do
   erb :create_payment
 end
 
 post '/payments/add' do
-  "Payment Created!" + params[:amount] + ", " + params[:currency] + ", " + params[:recipientid] + "."
-  # erb :confirm_payment, :locals => {:amount => params[:amount], :currency => params[:currency], :recipientid => params[:recipientid]}
+  paymentHandler = PaymentHandler.new
+  @ans = paymentHandler.send_payment(@@auth, params[:amount], params[:currency], params[:recipientid])
+  erb :confirm_payment
 end
--- t_irb.rb
+++
@@ -1,4 +0,0 @@
-# require "./lib/auth"
-# require "./lib/payment_handler"
-# a = PaymentHandler.new
-# b = a.send_payment(10.5, "USD", "69401e87-182e-4cf7-b4a4-895da78baa20")
